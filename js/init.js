3ec1ede5532846d61214ed18faf80219f11df25b
Kelvin Man <kelvinman.tk@gmail.com>
2017-11-10 02:49:13 -0800

    changed dropdown item text color

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
 	    $(".dropdown-button").dropdown({belowOrigin: true,});
         $('select').material_select();
         $('.collapsible').collapsible();
+        $(".dropdown-content>li>a").css("color", "black");
 
 	})
 })(jQuery);
