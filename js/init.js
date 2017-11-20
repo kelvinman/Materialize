6f410972a38ca6177d71a4a765518733cdda1e93
Kelvin Man <kelvinman.tk@gmail.com>
2017-11-20 15:20:15 -0800

    cleaned up before merging with adigami back end

--- js/init.js
+++ js/init.js
@@ -1,11 +1,9 @@
 (function($){
 	$(function(){
-
 	    $('.button-collapse').sideNav();
 	    $(".dropdown-button").dropdown({belowOrigin: true,});
-        $('select').material_select();
-        $('.collapsible').collapsible();
         $(".dropdown-content>li>a").css("color", "black");
+        $('select').material_select();
 	})
 })(jQuery);
 
